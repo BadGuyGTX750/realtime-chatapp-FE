78a12927ecf3a53dc86915d3f7fbc33d953c1fad
BadGuyGTX750 <stefanstefan211@gmail.com>
2023-04-22 19:29:19 +0300

    removed unnecessary console.log()

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 import Router from './Router';
 
 const router = Router()
-console.log(router)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
