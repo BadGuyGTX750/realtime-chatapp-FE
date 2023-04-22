439b6e0a7331cd5520e19dcd92e24b3afaf31eb2
BadGuyGTX750 <stefanstefan211@gmail.com>
2023-04-22 19:29:51 +0300

    removed semicolon after each instruction for consitency

--- src/api/api_contact_login.js
+++ src/api/api_contact_login.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import appsettings from "../appsettings.json"
 
 export default async function api_contact_login(e, p) {
-  var serverUrl = appsettings.serverUrl;
+  var serverUrl = appsettings.serverUrl
   var payload = {
     email: e,
     password: p
-  };
+  }
   return await axios.post(
     serverUrl + "/api/contact/login",
     payload,
