8a64d3b0827a61cf2c5dc170f180a1d680e27106
Anca Maria <stefanstefan211@gmail.com>
2023-04-23 20:10:51 +0300

    Worked a lil bit on conv page

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,9 +1,13 @@
-
+import "./ChatPage.css"
+import Navbar from "../components/navbar/Navbar"
 
 const ChatPage = () => {
   return (
-    <div>
-      <h1>CHAT PAGEEEEEEEEEEE</h1>
+    <div className="conv-page">
+      <Navbar height="127px"/>
+      <div className="conv-page-wrapper">
+        
+      </div>
     </div>
   )
 }
