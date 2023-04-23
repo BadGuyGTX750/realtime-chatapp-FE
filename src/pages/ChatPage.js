776a6e0c74b7fd5cce23c456ab2dcb321416497c
Anca Maria <stefanstefan211@gmail.com>
2023-04-23 21:39:15 +0300

    massive improvements of ConversationPage layout

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -6,7 +6,28 @@ const ChatPage = () => {
     <div className="conv-page">
       <Navbar height="127px"/>
       <div className="conv-page-wrapper">
-        
+        <div className="conv-page-conv-list">
+          <div className="conv-page-conv-list-header">
+
+          </div>
+          <div className="conv-page-conv-list-search">
+
+          </div>
+          <div clasName="conv-page-conv-list-conversations">
+
+          </div>
+        </div>
+        <div className="conv-page-conv-window">
+          <div className="conv-page-conv-window-header">
+
+          </div>
+          <div className="conv-page-conv-window-messages">
+
+          </div>
+          <div className="conv-page-conv-window-type">
+
+          </div>
+        </div>
       </div>
     </div>
   )
